d51caef852d9421945348043e6c5162c9e453787
kostas <justin.dekeyser@gmail.com>
2020-11-14 09:18:01 +0100

    [ADMIN] Allow append last in chunk to be O(1)

--- chunkmonad.c
+++ chunkmonad.c
@@ -1,10 +1,17 @@
 #include <stdlib.h>
 #include "chunkmonad.h"
 
+typedef struct ChunkLastMnemoized ChunkLastMnemoized;
+
 struct ChunkZip {
 	int restore;
 	void* ref;
 	ChunkZip* next;
+	ChunkLastMnemoized* lastIndirRef;
+};
+
+struct ChunkLastMnemoized {
+	ChunkZip* ref;
 };
 
 void* getvalue(ChunkZip* chunk) {
@@ -17,10 +24,22 @@ int restorepoint(ChunkZip* chunk) {
 
 ChunkZip* unit(void* ref) {
 	ChunkZip* chunk = malloc(sizeof(ChunkZip));
+	ChunkLastMnemoized* lastIndirRef = malloc(sizeof(ChunkLastMnemoized));
 	chunk -> restore = 0;
 	chunk -> ref = ref;
 	chunk -> next = (void*)0;
+	chunk -> lastIndirRef = lastIndirRef;
+	lastIndirRef -> ref = chunk;
 	return chunk;
+}
+
+static ChunkZip* dispose_and_returnnext(ChunkZip* chunk) {
+	ChunkZip* next = chunk -> next;
+	if (!next) {
+		free(chunk -> lastIndirRef);
+	}
+	free(chunk);
+	return next;
 }
 
 ChunkZip* moveforward(ChunkZip* chunk, NextVisitor nextVisitor, DisposeVisitor disposeVisitor) { 
@@ -32,22 +51,20 @@ ChunkZip* moveforward(ChunkZip* chunk, NextVisitor nextVisitor, DisposeVisitor d
 			return moveforward(chunk,nextVisitor,disposeVisitor);
 		}
 		return chunk;
-	} else {
-		ChunkZip* next = chunk -> next;
-		free(chunk);
-		return next;
-	}
+	} else return dispose_and_returnnext(chunk);
 }
 
 void emitevent(ChunkZip* lastNonNull, void* event, int restore) {
-	while(lastNonNull -> next) {
+	/*while(lastNonNull -> next) {
 		lastNonNull = lastNonNull -> next;
-	}
+	}*/
 
 	ChunkZip* newChunk = malloc(sizeof(ChunkZip));
 	newChunk -> restore = restore;
 	newChunk -> ref = event;
+	newChunk -> lastIndirRef = lastNonNull -> lastIndirRef;
 
-	lastNonNull -> next = newChunk;
+	lastNonNull -> lastIndirRef -> ref -> next = newChunk;
+	lastNonNull -> lastIndirRef -> ref = newChunk;
 }
 
